7a86fbe6414488421f8fc2b954eb6b6f55f21d10
Andrea030105 <tummoloandrea05@gmail.com>
2026-03-18 13:23:48 +0100

    ex-done

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,7 +1,54 @@
 @extends('layout.app')
 
 @section('content')
-<h1>
-    cerate sectuon
-</h1>
+<div class="container">
+    <div class="row">
+        <div class="col m-3">
+            <form action="{{ route( 'comics.store' ) }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="slug" class="form-label">Slug</label>
+                    <input type="text" class="form-control" id="slug" name="slug">
+                </div>
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="mb-3">
+                    <label for="thumb" class="form-label">Thumb</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" rows="3" id="description" name="description"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="price" class="form-label">Price</label>
+                    <input type="number" class="form-control" id="price" name="price"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="series" class="form-label">Series</label>
+                    <input type="text" class="form-control" id="series" name="series">
+                </div>
+                <div class="mb-3">
+                    <label for="type" class="form-label">Type</label>
+                    <input type="text" class="form-control" id="type" name="type">
+                </div>
+                <div class="mb-3">
+                    <label for="artists" class="form-label">Artists</label>
+                    <input type="text" class="form-control" id="artists" name="artists">
+                </div>
+                <div class="mb-3">
+                    <label for="writers" class="form-label">Writers</label>
+                    <input type="text" class="form-control" id="writers" name="writers">
+                </div>
+                <div class="mb-3">
+                    <label for="sale_date" class="form-label">Sale Date</label>
+                    <input type="date" class="form-control" id="sale_date" name="sale_date">
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
